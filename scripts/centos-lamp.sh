97c56051f23b05a15c3885c91fb069c6880d9b3c
Surikesh <40427203+Surikesh@users.noreply.github.com>
2021-12-31 20:00:21 +0300

    changed repository instalation type

--- scripts/centos-lamp.sh
+++ scripts/centos-lamp.sh
@@ -1,11 +1,10 @@
 #!/bin/bash
 
-rpm -Uvh http://dl.fedoraproject.org/pub/epel/7/x86_64/Packages/e/epel-release-7-11.noarch.rpm
-rpm -Uvh http://rpms.famillecollet.com/enterprise/remi-release-7.rpm
+sudo yum install -y epel-release 
 
 #install apache
 
-yum --enablerepo=epel,remi install -y httpd
+yum install -y httpd
 systemctl enable httpd.service
 
 # sudo apt-get install -y nano git unzip screen apache2
@@ -17,13 +16,12 @@ systemctl start httpd.service
 # sudo apt-mark unhold linux-image-generic linux-headers-generic
 
 # install php
-yum --enablerepo=epel,remi-php74 install -y php
-yum --enablerepo=remi-php74 list php-*
-yum --enablerepo=remi-php74 install -y php-mysql php-xml php-xmlrpc php-soap php-gd
+yum install -y php
+yum list php-*
+yum install -y php-mysql php-xml php-xmlrpc php-soap php-gd
 # sudo apt install -y php libapache2-mod-php php-mysql
 
 #install mysql
-rpm -Uvh  https://repo.mysql.com/mysql80-community-release-el7-1.noarch.rpm
 yum install -y mysql-server
 # sudo apt install -y mysql-server
 systemctl enable mysql
